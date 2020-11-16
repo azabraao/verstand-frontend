db0dc152975c3a0bb06c9ec2717866da81aadc90
azabraao <az.abraao@gmail.com>
2020-11-15 22:55:30 -0300

    wip login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,21 @@
 import React, { memo } from "react";
+import Input from "../../components/Form/Input";
+import Button from "../../components/Button";
 import LoginStyle from "./styles";
 
 const Login = () => {
   return (
     <LoginStyle>
-      <h1>Verstans</h1>
+      <div className="container">
+        <h1>Verstand</h1>
+        <h2>Login</h2>
+
+        <Input type="email" name="email" id="email" label="E-mail address" />
+        <Input type="password" name="senha" id="senha" label="Senha" />
+        <Button>
+          Sign in
+        </Button>
+      </div>
     </LoginStyle>
   );
 };
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,7 +2,25 @@ import styled from "styled-components";
 
 const LoginStyle = styled.section`
   background: #000;
+  min-height: 100vh;
   color: #ffffff;
+  padding-top: 48px;
+
+  h1 {
+    font-size: 36px;
+    line-height: 45px;
+    text-align: center;
+    color: #ffffff;
+    padding-bottom: 56px;
+  }
+
+  h2 {
+    font-size: 32px;
+    line-height: 31px;
+    text-align: center;
+    color: #ffffff;
+    margin-bottom: 40px;
+  }
 `;
 
 export default LoginStyle;
