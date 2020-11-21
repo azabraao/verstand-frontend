cfc0bb034bc48e844190a11c019afc14cc902677
azabraao <az.abraao@gmail.com>
2020-11-20 21:45:31 -0300

    add easter egg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import GlobalStyles from "./styles";
 import Routes from "./routes";
+import React, { useEffect } from "react";
+import EasterEgg from "./common/easter-egg";
 
 function App() {
+  useEffect(() => {
+    EasterEgg();
+  }, []);
   return (
     <>
       <GlobalStyles />
---
+++ src/common/easter-egg.js
@@ -0,0 +1,21 @@
+const EasterEgg = () => {
+console.log(
+`
+██╗   ██╗███████╗██████╗ ███████╗████████╗ █████╗ ███╗   ██╗██████╗ 
+██║   ██║██╔════╝██╔══██╗██╔════╝╚══██╔══╝██╔══██╗████╗  ██║██╔══██╗
+██║   ██║█████╗  ██████╔╝███████╗   ██║   ███████║██╔██╗ ██║██║  ██║
+╚██╗ ██╔╝██╔══╝  ██╔══██╗╚════██║   ██║   ██╔══██║██║╚██╗██║██║  ██║
+ ╚████╔╝ ███████╗██║  ██║███████║   ██║   ██║  ██║██║ ╚████║██████╔╝
+  ╚═══╝  ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ 
+`,
+`
+
+Made with much fun by the follow by the following folks:
+https://github.com/azabraao
+https://github.com/rodsussumu
+https://github.com/geovannecoelho
+https://github.com/iago-monteirog
+`);
+};
+
+export default EasterEgg
